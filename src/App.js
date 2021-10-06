fdc182ff535bfc8130d34c8abd7b5f200ce693ee
Lynn M <78457709+Giggles999@users.noreply.github.com>
2021-10-05 18:34:32 -0700

    updated search function

--- src/App.js
+++ src/App.js
@@ -5,104 +5,88 @@ import Overview from "./Overview";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1> Weather App Week 4 Homework </h1>
+
+<div className="App">
+  <header className="App-header">
+    <h1> Weather App Week 4 Homework </h1>
         <Search />
 
-        <div class="row mt-5">
-        <div class="col-md-6">
-          <h2>Seattle</h2>
-          <p>
-            Tuesday October 5, 2021
-            <br />
-            Description: Partly Cloudy
-            <br />
-            Humidity: 60% <br />
-            Wind: 4 km/h <br />
-            <br />
-          </p>
-        </div>
-        <div class="col-md-6">
-          <h2>Seattle</h2>
-          <p>
-            <ul>
-              <li>
-                <Overview temperature={5} />{" "}
-              </li>
-              <li>Description: Partly Cloudy</li>
-              <li>Humidity: 60% </li>
-              <li>Wind: 4 km/h</li>
-            </ul>
-          </p>
-        </div>
-      </div>
-      </header>
+<div class="row mt-5">
+  <div class="col-md-6">
+    <h2>Seattle</h2>
+    <p><ul>
+      <li>Tuesday October 5, 2021 </li>
+      <li>Description: Partly Cloudy </li>
+      <li>Humidity: 60% </li>
+      <li>Wind: 4 km/h </li>
+    </ul></p>
+</div>
+     
+<div class="col-md-6">
+  <h2>Seattle</h2>
+    <p><ul>
+      <li><Overview temperature={5} />{" "} </li>
+      <li>Description: Partly Cloudy</li>
+      <li>Humidity: 60% </li>
+      <li>Wind: 4 km/h</li>
+    </ul> </p>
+</div>
+</div>
+</header>
+
 <body className="App-body">
-            <h2>Forecast</h2>
-            <div class="row mt-5">
-            <div class="col-md-2">
-              <p>
-                <ul>
-                  <li>Day 1</li>
-                  <li>Image</li>
-                  <li>Temp F|C</li>
-                </ul>
-              </p>
-            </div>
+  <h2>Forecast</h2>
 
-            <div class="col-md-2">
-              <p>
-                <ul>
-                  <li>Day 2</li>
-                  <li>Image</li>
-                  <li>Temp F|C</li>
-                </ul>
-              </p>
-            </div>
+<div class="row mt-5">
+  <div class="col-md-2">
+    <p><ul>
+      <li>Day 1</li>
+      <li>Image</li>
+      <li>Temp F|C</li>
+    </ul></p>
+  </div>
 
-            <div class="col-md-2">
-              <p>
-                <ul>
-                  <li>Day 3</li>
-                  <li>Image</li>
-                  <li>Temp F|C</li>
-                </ul>
-              </p>
-            </div>
+  <div class="col-md-2">
+    <p><ul>
+      <li>Day 2</li>
+      <li>Image</li>
+      <li>Temp F|C</li>
+    </ul></p>
+  </div>
 
-            <div class="col-md-2">
-              <p>
-                <ul>
-                  <li>Day 4</li>
-                  <li>Image</li>
-                  <li>Temp F|C</li>
-                </ul>
-              </p>
-            </div>
+  <div class="col-md-2">
+    <p><ul>
+      <li>Day 3</li>
+      <li>Image</li>
+      <li>Temp F|C</li>
+    </ul></p>
+  </div>
 
-            <div class="col-md-2">
-              <p>
-                {" "}
-                <ul>
-                  <li>Day 5</li>
-                  <li>Image</li>
-                  <li>Temp F|C</li>
-                </ul>
-              </p>
-            </div>
+  <div class="col-md-2">
+    <p><ul>
+      <li>Day 4</li>
+      <li>Image</li>
+      <li>Temp F|C</li>
+    </ul></p>
+  </div>
 
-            <div class="col-md-2">
-              <p>
-                {" "}
-                <ul>
-                  <li>Day 6</li>
-                  <li>Image</li>
-                  <li>Temp F|C</li>
-                </ul>
-              </p>
-              </div>
-       </div>
+  <div class="col-md-2">
+    <p><ul>
+      <li>Day 5</li>
+      <li>Image</li>
+      <li>Temp F|C</li>
+    </ul></p>
+  </div>
+
+  <div class="col-md-2">
+    <p><ul>
+      <li>Day 6</li>
+      <li>Image</li>
+      <li>Temp F|C</li>
+    </ul></p>
+  </div>
+
+</div>
 </body>
       <Footer />
     </div>
--- src/Search.js
+++ src/Search.js
@@ -1,37 +1,42 @@
 import React, { useState } from "react";
 import axios from "./Api";
 
-export default function WeatherSearch() {
+export default function Search() {
   const [city, setCity] = useState("");
+  const [weather, setWeatherConditions] = useState({});
   const [loaded, setLoaded] = useState(false);
-  const [weather, setWeather] = useState({});
 
-  function displayWeather(response) {
+  function displayWeatherConditions(response) {
     setLoaded(true);
-    setWeather({
+    setWeatherConditions({
       temperature: response.data.main.temp,
       wind: response.data.wind.speed,
       humidity: response.data.main.humidity,
-      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       description: response.data.weather[0].description,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
     });
   }
 
-  function handleSubmit(event) {
+  function handleSearch(event) {
     event.preventDefault();
-    let apiKey = "6f65ac3695a44ef64022cd653378b553";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(displayWeather);
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=6f65ac3695a44ef64022cd653378b553&units=imperial`;
+    axios.get(apiUrl).then(displayWeatherConditions);
   }
 
   function updateCity(event) {
+    event.preventDefault();
     setCity(event.target.value);
   }
 
   let form = (
-    <form onSubmit={handleSubmit}>
-      <input type="search" placeholder="Enter a city.." onChange={updateCity} />
-      <button type="Submit">Search</button>
+    <form onSubmit={handleSearch}>
+      <input
+        type="text"
+        placeholder="Enter a city.."
+        autoFocus={true}
+        onChange={updateCity}
+      />
+      <input type="submit" value="search" />
     </form>
   );
 
@@ -39,13 +44,18 @@ export default function WeatherSearch() {
     return (
       <div>
         {form}
+
+        <h3> In {city} we have: </h3>
+
         <ul>
-          <li>Temperature: {Math.round(weather.temperature)}°C</li>
-          <li>Description: {weather.description}</li>
-          <li>Humidity: {weather.humidity}%</li>
-          <li>Wind: {weather.wind}km/h</li>
+          <li> Temperature: {Math.round(weather.temperature)}°F </li>
+          <li> Humidity: {weather.humidity}%</li>
+          <li> Description: {weather.description} </li>
+          <li> Wind: {Math.round(weather.wind)} kmh </li>
+
           <li>
-            <img src={weather.icon} alt={weather.description} />
+            {" "}
+            <img src={weather.icon} alt={weather.description} />{" "}
           </li>
         </ul>
       </div>
